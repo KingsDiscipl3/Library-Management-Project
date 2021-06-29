b9b443e85a34eddec622c71f5873ed7ad3244b07
KingsDiscipl3 <65213015+KingsDiscipl3@users.noreply.github.com>
2021-06-28 20:37:03 -0700

    Create LibManPro.cpp

---
+++ LibManPro.cpp
@@ -0,0 +1,17 @@
+// Library Management Project
+
+// Header Files Included in Project
+
+#include <fstream.h>
+#include <conio.h>
+#include <stdio.h>
+#include <process.h>
+#include <string.h>
+#include <iomap.h>
+
+// Classes in the Project
+
+class book 
+{
+
+}
